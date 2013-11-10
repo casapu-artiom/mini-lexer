83c86f250e1a1244a89e39dc671d7787493694fd
Cristian Vasilache <Cristian.Vasilache@iquestgroup.com>
2013-11-10 18:04:30 +0200

    Initial commit

---
+++ Lexer.py
@@ -0,0 +1,30 @@
+__author__ = 'Artiom.Casapu'
+
+token_definition = {
+    '': (''),
+    '': (''),
+    '': (''),
+    '': (''),
+    '': (''),
+    '': (''),
+    '': (''),
+    '': (''),
+    '': (''),
+}
+
+class Lexer:
+
+    def Lexer(self, token_definition):
+        pass
+
+    def getSymbolTableAndProgramStructure(self, input):
+        pass
+
+    pass
+
+input1 = """
+"""
+
+input2 = """
+"""
+
